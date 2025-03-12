c841808ee0344cb7db38e81b4182e7613de5d9a8
eyyupsinbahar <eyyupsinbahar@gmail.com>
2025-03-12 09:57:51 +0000

    Bottom Navigation Bar Düzenlendi.

--- lib/home.dart
+++ lib/home.dart
@@ -8,24 +8,51 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  int _selectedIndex = 0;
+
+  static List<Widget> pages = [
+    Container(color: Colors.red),
+    Container(color: Colors.green),
+    Container(color: Colors.blue),
+  ];
+
+  void _onItemTabbet(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          elevation: 2.0,
-          centerTitle: true,
-          title: Text(
-            'ŞenSoft Yemek Uygulaması',
-            style: Theme.of(context).textTheme.headlineLarge,
-          ),
+      appBar: AppBar(
+        elevation: 2.0,
+        centerTitle: true,
+        title: Text(
+          'ŞenSoft Yemek Uygulaması',
+          style: Theme.of(context).textTheme.headlineLarge,
         ),
-        body: Center(
-          child: Text(
-            'Hadi Yemek Pişirelim!',
-            style: Theme.of(context).textTheme.headlineMedium,
+      ),
+      body: pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.card_giftcard),
+            label: "Card",
           ),
-        ),
-        // backgroundColor: Colors. white,
-      );
+          BottomNavigationBarItem(
+            icon: Icon(Icons.card_giftcard),
+            label: "Card2",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.card_giftcard),
+            label: "Card3",
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: _onItemTabbet,
+      ),
+      // backgroundColor: Colors. white,
+    );
   }
 }
